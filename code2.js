2e45485c7cf71704cb9722d2410bea23007b79f7
Digoska <104101196+Digoska@users.noreply.github.com>
2025-01-04 01:57:50 +0100

    v9

---
+++ code2.js
@@ -0,0 +1,54 @@
+gdjs.surshcillCode = {};
+gdjs.surshcillCode.localVariables = [];
+gdjs.surshcillCode.GDNewTextObjects1= [];
+gdjs.surshcillCode.GDNewTextObjects2= [];
+gdjs.surshcillCode.GDRedButtonWithShadowObjects1= [];
+gdjs.surshcillCode.GDRedButtonWithShadowObjects2= [];
+
+
+gdjs.surshcillCode.eventsList0 = function(runtimeScene) {
+
+{
+
+gdjs.copyArray(runtimeScene.getObjects("RedButtonWithShadow"), gdjs.surshcillCode.GDRedButtonWithShadowObjects1);
+
+let isConditionTrue_0 = false;
+isConditionTrue_0 = false;
+for (var i = 0, k = 0, l = gdjs.surshcillCode.GDRedButtonWithShadowObjects1.length;i<l;++i) {
+    if ( gdjs.surshcillCode.GDRedButtonWithShadowObjects1[i].IsPressed((typeof eventsFunctionContext !== 'undefined' ? eventsFunctionContext : undefined)) ) {
+        isConditionTrue_0 = true;
+        gdjs.surshcillCode.GDRedButtonWithShadowObjects1[k] = gdjs.surshcillCode.GDRedButtonWithShadowObjects1[i];
+        ++k;
+    }
+}
+gdjs.surshcillCode.GDRedButtonWithShadowObjects1.length = k;
+if (isConditionTrue_0) {
+{gdjs.evtTools.window.openURL("https://www.youtube.com/watch?v=xvFZjo5PgG0", runtimeScene);
+}{gdjs.evtTools.runtimeScene.stopGame(runtimeScene);
+}}
+
+}
+
+
+};
+
+gdjs.surshcillCode.func = function(runtimeScene) {
+runtimeScene.getOnceTriggers().startNewFrame();
+
+gdjs.surshcillCode.GDNewTextObjects1.length = 0;
+gdjs.surshcillCode.GDNewTextObjects2.length = 0;
+gdjs.surshcillCode.GDRedButtonWithShadowObjects1.length = 0;
+gdjs.surshcillCode.GDRedButtonWithShadowObjects2.length = 0;
+
+gdjs.surshcillCode.eventsList0(runtimeScene);
+gdjs.surshcillCode.GDNewTextObjects1.length = 0;
+gdjs.surshcillCode.GDNewTextObjects2.length = 0;
+gdjs.surshcillCode.GDRedButtonWithShadowObjects1.length = 0;
+gdjs.surshcillCode.GDRedButtonWithShadowObjects2.length = 0;
+
+
+return;
+
+}
+
+gdjs['surshcillCode'] = gdjs.surshcillCode;
